b770f1a2713d0a3643368bebf1ac4e908d955ae7
Zelina974 <155895303+Zelina974@users.noreply.github.com>
2024-01-07 15:24:45 +0100

    erreurs corrigées

--- ftc2024-carlike.java
+++ ftc2024-carlike.java
@@ -16,15 +16,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Werobot_FTC2024_carlike extends LinearOpMode {
         private DcMotor rm;
         private DcMotor lm;
-	private double helloexp(double t){
-		return (Math.exp(5*t)-1)/(Math.exp(5)-1);
-	}
+    private double helloexp(double t){
+        return (Math.exp(5*t)-1)/(Math.exp(5)-1);
+    }
         @Override
         public void runOpMode() throws InterruptedException {
                 float x;
                 double y;
                 double t;
-		double t2;
+                double t2;
                 double t3;
                 String mode = "normal";
                 boolean already_a = false;
@@ -32,7 +32,7 @@ public class Werobot_FTC2024_carlike extends LinearOpMode {
                 telemetry.update();
                 lm = hardwareMap.get(DcMotor.class, "blm");
                 rm = hardwareMap.get(DcMotor.class, "brm");
-		rm.setDirection(DcMotorSimple.Direction.REVERSE);
+        rm.setDirection(DcMotorSimple.Direction.REVERSE);
                 waitForStart();
                 
                 
@@ -40,17 +40,17 @@ public class Werobot_FTC2024_carlike extends LinearOpMode {
                         x = gamepad1.left_stick_x;
                         y = gamepad1.left_stick_y;
                         t= gamepad1.right_trigger;
-			t2 = helloexp(t);
-                        t3 = helloexp(Math.sqrt(x^2+y^2))
+                        t2 = helloexp(t);
+                        t3 = helloexp(Math.sqrt(Math.pow(x,2)+Math.pow(y,2)));
                         telemetry.addData("Status", "Running");
                         if(gamepad1.a && !already_a){
                                 if(mode=="normal"){
                                         mode="tank";
                                 }else if(mode=="tank"){
-					mode = "essaifranck";
+                    mode = "essaifranck";
                                 }else if (mode == "essaifranck"){
                                         mode = "elina";
-				}else{
+                }else{
                                         mode="normal";
                                 }
                                 already_a = true;
@@ -70,30 +70,31 @@ public class Werobot_FTC2024_carlike extends LinearOpMode {
                                 lpower = -y;
                                 rpower = gamepad1.right_stick_y;
                         }
-			else if (mode=="essaifranck"){
-				double a = (-y+x)/Math.pow(2,1/2);
-				double b = (-y-x)/Math.pow(2,1/2);
+            else if (mode=="essaifranck"){
+                double a = (-y+x)/Math.pow(2,1/2);
+                double b = (-y-x)/Math.pow(2,1/2);
                                 double vmean = (Math.abs(a)+Math.abs(b))/2;
                                 lpower = (a/vmean)*t2;
                                 rpower = (b/vmean)*t2;
-			}
+            }
                         else if (mode=="elina"){
                                 double a = (-y+x)/Math.pow(2,1/2);
-				double b = (-y-x)/Math.pow(2,1/2);
+                double b = (-y-x)/Math.pow(2,1/2);
                                 double vmean = (Math.abs(a)+Math.abs(b))/2;
-                                lpower = (a/vmean)*t3
-                                rpower = (b/vmean)*t3
+                                lpower = (a/vmean)*t3;
+                                rpower = (b/vmean)*t3;
+                        }
                         if(!(gamepad1.left_bumper)){
                                 lpower/=3;
                                 rpower/=3;
                         }
                         lm.setPower(lpower);
                         rm.setPower(rpower);
-			
-			telemetry.addData("x",x);
-			telemetry.addData("y",y);
-			telemetry.addData("lpow",lpower);
-			telemetry.addData("rpow",rpower);
+            
+            telemetry.addData("x",x);
+            telemetry.addData("y",y);
+            telemetry.addData("lpow",lpower);
+            telemetry.addData("rpow",rpower);
                         telemetry.addData("ltrigg",t);
                         telemetry.addData("t2",t2);
                         telemetry.addData("mode",mode);
