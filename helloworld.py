ce714d1656610e4ba7cffa3acad2f15bb67dd4a6
GZod01 <gzod01@gzod01.fr>
2024-02-04 16:14:23 +0100

    new:commit

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+abc = """
+boolean already_//CURR// = false;
+if(gamepad1.//CURR// && !already_//CURR//){
+    already_//CURR// =! already_//CURR//;
+    motor_n_//CURR//.setPower(1);
+}
+if (!gamepad1.//CURR// && already_//CURR//){
+    already_//CURR// =! already_//CURR//;
+}
+
+""";
+defg = "";
+
+hijk = "abcdefghijklmnopqrstuvwxyz";
+
+for i in hijk:
+    defg += abc.replace("//CURR//",i);
+
+print(defg);
---
+++ helloworld.py.res.java
@@ -0,0 +1,261 @@
+
+boolean already_a = false;
+if(gamepad1.a && !already_a){
+    already_a =! already_a;
+    motor_n_a.setPower(1);
+}
+if (!gamepad1.a && already_a){
+    already_a =! already_a;
+}
+
+
+boolean already_b = false;
+if(gamepad1.b && !already_b){
+    already_b =! already_b;
+    motor_n_b.setPower(1);
+}
+if (!gamepad1.b && already_b){
+    already_b =! already_b;
+}
+
+
+boolean already_c = false;
+if(gamepad1.c && !already_c){
+    already_c =! already_c;
+    motor_n_c.setPower(1);
+}
+if (!gamepad1.c && already_c){
+    already_c =! already_c;
+}
+
+
+boolean already_d = false;
+if(gamepad1.d && !already_d){
+    already_d =! already_d;
+    motor_n_d.setPower(1);
+}
+if (!gamepad1.d && already_d){
+    already_d =! already_d;
+}
+
+
+boolean already_e = false;
+if(gamepad1.e && !already_e){
+    already_e =! already_e;
+    motor_n_e.setPower(1);
+}
+if (!gamepad1.e && already_e){
+    already_e =! already_e;
+}
+
+
+boolean already_f = false;
+if(gamepad1.f && !already_f){
+    already_f =! already_f;
+    motor_n_f.setPower(1);
+}
+if (!gamepad1.f && already_f){
+    already_f =! already_f;
+}
+
+
+boolean already_g = false;
+if(gamepad1.g && !already_g){
+    already_g =! already_g;
+    motor_n_g.setPower(1);
+}
+if (!gamepad1.g && already_g){
+    already_g =! already_g;
+}
+
+
+boolean already_h = false;
+if(gamepad1.h && !already_h){
+    already_h =! already_h;
+    motor_n_h.setPower(1);
+}
+if (!gamepad1.h && already_h){
+    already_h =! already_h;
+}
+
+
+boolean already_i = false;
+if(gamepad1.i && !already_i){
+    already_i =! already_i;
+    motor_n_i.setPower(1);
+}
+if (!gamepad1.i && already_i){
+    already_i =! already_i;
+}
+
+
+boolean already_j = false;
+if(gamepad1.j && !already_j){
+    already_j =! already_j;
+    motor_n_j.setPower(1);
+}
+if (!gamepad1.j && already_j){
+    already_j =! already_j;
+}
+
+
+boolean already_k = false;
+if(gamepad1.k && !already_k){
+    already_k =! already_k;
+    motor_n_k.setPower(1);
+}
+if (!gamepad1.k && already_k){
+    already_k =! already_k;
+}
+
+
+boolean already_l = false;
+if(gamepad1.l && !already_l){
+    already_l =! already_l;
+    motor_n_l.setPower(1);
+}
+if (!gamepad1.l && already_l){
+    already_l =! already_l;
+}
+
+
+boolean already_m = false;
+if(gamepad1.m && !already_m){
+    already_m =! already_m;
+    motor_n_m.setPower(1);
+}
+if (!gamepad1.m && already_m){
+    already_m =! already_m;
+}
+
+
+boolean already_n = false;
+if(gamepad1.n && !already_n){
+    already_n =! already_n;
+    motor_n_n.setPower(1);
+}
+if (!gamepad1.n && already_n){
+    already_n =! already_n;
+}
+
+
+boolean already_o = false;
+if(gamepad1.o && !already_o){
+    already_o =! already_o;
+    motor_n_o.setPower(1);
+}
+if (!gamepad1.o && already_o){
+    already_o =! already_o;
+}
+
+
+boolean already_p = false;
+if(gamepad1.p && !already_p){
+    already_p =! already_p;
+    motor_n_p.setPower(1);
+}
+if (!gamepad1.p && already_p){
+    already_p =! already_p;
+}
+
+
+boolean already_q = false;
+if(gamepad1.q && !already_q){
+    already_q =! already_q;
+    motor_n_q.setPower(1);
+}
+if (!gamepad1.q && already_q){
+    already_q =! already_q;
+}
+
+
+boolean already_r = false;
+if(gamepad1.r && !already_r){
+    already_r =! already_r;
+    motor_n_r.setPower(1);
+}
+if (!gamepad1.r && already_r){
+    already_r =! already_r;
+}
+
+
+boolean already_s = false;
+if(gamepad1.s && !already_s){
+    already_s =! already_s;
+    motor_n_s.setPower(1);
+}
+if (!gamepad1.s && already_s){
+    already_s =! already_s;
+}
+
+
+boolean already_t = false;
+if(gamepad1.t && !already_t){
+    already_t =! already_t;
+    motor_n_t.setPower(1);
+}
+if (!gamepad1.t && already_t){
+    already_t =! already_t;
+}
+
+
+boolean already_u = false;
+if(gamepad1.u && !already_u){
+    already_u =! already_u;
+    motor_n_u.setPower(1);
+}
+if (!gamepad1.u && already_u){
+    already_u =! already_u;
+}
+
+
+boolean already_v = false;
+if(gamepad1.v && !already_v){
+    already_v =! already_v;
+    motor_n_v.setPower(1);
+}
+if (!gamepad1.v && already_v){
+    already_v =! already_v;
+}
+
+
+boolean already_w = false;
+if(gamepad1.w && !already_w){
+    already_w =! already_w;
+    motor_n_w.setPower(1);
+}
+if (!gamepad1.w && already_w){
+    already_w =! already_w;
+}
+
+
+boolean already_x = false;
+if(gamepad1.x && !already_x){
+    already_x =! already_x;
+    motor_n_x.setPower(1);
+}
+if (!gamepad1.x && already_x){
+    already_x =! already_x;
+}
+
+
+boolean already_y = false;
+if(gamepad1.y && !already_y){
+    already_y =! already_y;
+    motor_n_y.setPower(1);
+}
+if (!gamepad1.y && already_y){
+    already_y =! already_y;
+}
+
+
+boolean already_z = false;
+if(gamepad1.z && !already_z){
+    already_z =! already_z;
+    motor_n_z.setPower(1);
+}
+if (!gamepad1.z && already_z){
+    already_z =! already_z;
+}
+
+
