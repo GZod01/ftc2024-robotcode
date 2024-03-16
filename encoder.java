a3b3c71d540ed17fad45ad81a1aba30cb1e94d76
Zelina974 <155895303+Zelina974@users.noreply.github.com>
2024-03-16 11:51:33 +0100

    Update encoder.java

--- encoder.java
+++ encoder.java
@@ -1,9 +1,9 @@
 DcMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // tare the encoder
-DcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+DcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);// bloque le motor
 // Position droite = 8 ticks
 // position initiale = 0 ticks
 // position allongée = - 16 ticks
 // in opMode:
-int pos_to_go = ...; // position to go
+int pos_to_go = 8; // position to go
 DcMotor.setTargetPosition(pos_to_go); // set target position, the motor will run to this pos
 DcMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); // mode run to position
