78fa73049738f8ae13d783b53916b154d105f86a
GZod01 <gzod01@gzod01.fr>
2024-04-01 15:54:34 +0200

    helloworld

---
+++ hello.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+import java.lang.String;
+public class hello{
+    public static void main(String[] args){
+	System.out.println(Arrays.toString(new Object[] {1, 1.2, 2.2984092840294, "helloworld"}));
+    }
+}
