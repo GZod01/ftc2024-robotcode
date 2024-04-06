eff1e655fe57c353a76bdfb767684d76578a6873
Zelina974 <elina.claerebout@gmail.com>
2024-04-06 14:17:08 +0200

    helloworld XD (when someone who is not Zelina make a commit...)

--- ftc2024_auto_r2d.java
+++ ftc2024_auto_r2d.java
@@ -1,7 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+import org.firstinspires.ftc.teamcode.Ftc2024_autonome_api;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-       package org.firstinspires.ftc.teamcode;
-       import org.firstinspires.ftc.teamcode.Ftc2024_autonome_api;
-       public class ftc2024_auto_r2d extends Ftc2024_autonome_api{
-           public AutoMode autonomous_mode = AutoMode.R2D;
-       }
+@Autonomous
+public class ftc2024_auto_r2d extends LinearOpMode{
+    //hi
+    public Ftc2024_autonome_api.AutoMode autonomous_mode = Ftc2024_autonome_api.AutoMode.R2D;
+    
+    @Override
+    public void runOpMode() {
+        Ftc2024_autonome_api a = new Ftc2024_autonome_api();
+        a.hardwareMap = this.hardwareMap;
+        a.runOpMode();
+    }
+}
        
